524485cdb19f347a7e4e3f1387d33325a73edb52
WakandianShield <santiagogallardojr@gmail.com>
2026-01-21 01:51:25 +0000

    BOTONES PERRONES

--- script.js
+++ script.js
@@ -13,3 +13,68 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled');
     }
 });
+
+
+
+
+
+
+  const images = document.querySelectorAll('.prev-pic img');
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio >= 0.5) {
+          entry.target.classList.add('active');
+        } else {
+          entry.target.classList.remove('active');
+        }
+      });
+    },
+    {
+      root: document.querySelector('.prev-pic'),
+      threshold: [0.5]
+    }
+  );
+
+  images.forEach(img => observer.observe(img));
+
+
+
+
+
+
+  const container = document.querySelector('.prev-pic');
+  const slides = document.querySelectorAll('.prev-pic img');
+
+  let index = 0;
+  let autoScroll;
+
+  function scrollToSlide(i) {
+    const slideWidth = slides[0].offsetWidth + parseFloat(getComputedStyle(container).gap);
+    container.scrollTo({
+      left: slideWidth * i,
+      behavior: 'smooth'
+    });
+  }
+
+  function startAutoScroll() {
+    autoScroll = setInterval(() => {
+      index = (index + 1) % slides.length;
+      scrollToSlide(index);
+    }, 3000);
+  }
+
+  function stopAutoScroll() {
+    clearInterval(autoScroll);
+  }
+
+  // Solo móvil
+  if (window.innerWidth <= 768) {
+    startAutoScroll();
+
+    // pausa si el usuario interactúa
+    container.addEventListener('touchstart', stopAutoScroll);
+    container.addEventListener('touchend', startAutoScroll);
+  }
+
